075b8b9cb8aeaa51acdc017b7cbcbf536e5cece8
RickCastroo <Heinrickoficial25@gmail.com>
2022-05-22 11:06:07 -0300

    Initial commit

---
+++ Teste Crud Cliente/Teste Crud Cliente/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Teste_Crud_Cliente
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxNumero = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btNovo = new System.Windows.Forms.Button();
+            this.btEditar = new System.Windows.Forms.Button();
+            this.btExcluir = new System.Windows.Forms.Button();
+            this.btConsultar = new System.Windows.Forms.Button();
+            this.btExibir = new System.Windows.Forms.Button();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBoxId
+            // 
+            this.txtBoxId.Location = new System.Drawing.Point(84, 15);
+            this.txtBoxId.Name = "txtBoxId";
+            this.txtBoxId.Size = new System.Drawing.Size(165, 23);
+            this.txtBoxId.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(50, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // txtBoxNome
+            // 
+            this.txtBoxNome.Location = new System.Drawing.Point(84, 44);
+            this.txtBoxNome.Name = "txtBoxNome";
+            this.txtBoxNome.Size = new System.Drawing.Size(165, 23);
+            this.txtBoxNome.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(22, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // txtBoxNumero
+            // 
+            this.txtBoxNumero.Location = new System.Drawing.Point(84, 73);
+            this.txtBoxNumero.Name = "txtBoxNumero";
+            this.txtBoxNumero.Size = new System.Drawing.Size(165, 23);
+            this.txtBoxNumero.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(7, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 21);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Numero:";
+            // 
+            // btNovo
+            // 
+            this.btNovo.Location = new System.Drawing.Point(84, 102);
+            this.btNovo.Name = "btNovo";
+            this.btNovo.Size = new System.Drawing.Size(75, 23);
+            this.btNovo.TabIndex = 2;
+            this.btNovo.Text = "Novo";
+            this.btNovo.UseVisualStyleBackColor = true;
+            // 
+            // btEditar
+            // 
+            this.btEditar.Location = new System.Drawing.Point(165, 102);
+            this.btEditar.Name = "btEditar";
+            this.btEditar.Size = new System.Drawing.Size(75, 23);
+            this.btEditar.TabIndex = 2;
+            this.btEditar.Text = "Editar";
+            this.btEditar.UseVisualStyleBackColor = true;
+            // 
+            // btExcluir
+            // 
+            this.btExcluir.Location = new System.Drawing.Point(246, 102);
+            this.btExcluir.Name = "btExcluir";
+            this.btExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btExcluir.TabIndex = 2;
+            this.btExcluir.Text = "Excluir";
+            this.btExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btConsultar
+            // 
+            this.btConsultar.Location = new System.Drawing.Point(327, 102);
+            this.btConsultar.Name = "btConsultar";
+            this.btConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btConsultar.TabIndex = 2;
+            this.btConsultar.Text = "Consultar";
+            this.btConsultar.UseVisualStyleBackColor = true;
+            // 
+            // btExibir
+            // 
+            this.btExibir.Location = new System.Drawing.Point(408, 102);
+            this.btExibir.Name = "btExibir";
+            this.btExibir.Size = new System.Drawing.Size(75, 23);
+            this.btExibir.TabIndex = 2;
+            this.btExibir.Text = "Exibir";
+            this.btExibir.UseVisualStyleBackColor = true;
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(84, 131);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.RowTemplate.Height = 25;
+            this.dgvDados.Size = new System.Drawing.Size(399, 190);
+            this.dgvDados.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 332);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.btExibir);
+            this.Controls.Add(this.btConsultar);
+            this.Controls.Add(this.btExcluir);
+            this.Controls.Add(this.btEditar);
+            this.Controls.Add(this.btNovo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxNumero);
+            this.Controls.Add(this.txtBoxNome);
+            this.Controls.Add(this.txtBoxId);
+            this.Name = "Form1";
+            this.Text = "CRUD Cliente";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtBoxId;
+        private Label label1;
+        private TextBox txtBoxNome;
+        private Label label2;
+        private TextBox txtBoxNumero;
+        private Label label3;
+        private Button btNovo;
+        private Button btEditar;
+        private Button btExcluir;
+        private Button btConsultar;
+        private Button btExibir;
+        private DataGridView dgvDados;
+    }
+}
---
+++ Teste Crud Cliente/Teste Crud Cliente/Form1.cs
@@ -0,0 +1,10 @@
+namespace Teste_Crud_Cliente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
